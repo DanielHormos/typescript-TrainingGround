26b7b45a11e5b1130a3bb1c2786c3fc2ae6fe320
Daniel Hormos <daniel_danne32@hotmail.com>
2024-09-12 18:17:49 +0200

    added interface interface

---
+++ interface.ts
@@ -0,0 +1,9 @@
+export interface IInterface {
+    name: string
+    birthYear: number
+    anotherProperty: number
+    anotherProperty1: string
+    anotherProperty2: number
+    anotherProperty3: Date
+    anotherProperty4: number
+}
